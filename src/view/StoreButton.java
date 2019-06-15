ea14a2a9b096b5084ae595ea8cf84db47713860f
harheem <49297157+harheem@users.noreply.github.com>
2019-06-15 21:32:19 +0900

    modify2 Store

--- src/view/StoreButton.java
+++ src/view/StoreButton.java
@@ -32,8 +32,9 @@ public class StoreButton extends JButton {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				ost.playmusic("./resourceFolder/sound/clickbgm.wav");
-				int num = JOptionPane.showConfirmDialog(null, "이 아이템을 구매하시겠습니까? 이 아이템은 " + item.getPrice() + "원 입니다.",
-						"구매확인창", JOptionPane.YES_NO_OPTION);
+				int num = JOptionPane.showConfirmDialog(null,
+						"이 아이템을 구매하시겠습니까? 이 아이템의 가격은 " + item.getPrice() + "원 입니다.", "구매확인창",
+						JOptionPane.YES_NO_OPTION);
 				if (num == 0) {
 					if (store.checkMoney(hunter, item) == true) {
 						JOptionPane.showMessageDialog(null, "구매가 완료되었습니다.");
@@ -50,7 +51,6 @@ public class StoreButton extends JButton {
 	public StoreButton(String name, int index) {
 		this.name = name;
 		this.index = index;
-		numAni = hunter.getNumAni()[index];
 		this.setSize(120, 120);
 		this.setBorderPainted(false);
 		this.setContentAreaFilled(false);
@@ -59,10 +59,13 @@ public class StoreButton extends JButton {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				ost.playmusic("./resourceFolder/sound/clickbgm.wav");
-				ani = hunter.sellAni(name);
-				// 확인창 이 동물을 파시겠습니까? 예/아니오 동물 마리수, 동물판매 후 잔액 나타내기
-				ani.getPrice();
-				int money = hunter.getMoney() - ani.getPrice();;
+				int num = JOptionPane.showConfirmDialog(null, "이 동물을 상점에 파시겠습니까?", "판매확인창", JOptionPane.YES_NO_OPTION);
+				if (num == 0) {
+					if (hunter.getNumAni()[index] > 0) {
+						hunter.sellAni(name);
+						JOptionPane.showMessageDialog(null, "판매가 완료되었습니다.");
+					} else JOptionPane.showMessageDialog(null, "현재 감옥에 이 동물이 없으므로 판매를 할 수가 없습니다!");
+				}
 			}
 		});
 	}
