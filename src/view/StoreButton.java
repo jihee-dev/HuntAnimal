72b8df5eb8f701b0cc0efcae807e15ba1319dd98
harheem <49297157+harheem@users.noreply.github.com>
2019-06-15 13:23:22 +0900

    change StoreView / Button

--- src/view/StoreButton.java
+++ src/view/StoreButton.java
@@ -19,19 +19,11 @@ public class StoreButton extends JButton implements ActionListener {
 	private Store store;
 	private Item item;
 	private Animal ani;
+	private String name;
 	private JLabel moneyLabel = new JLabel();
-
-	// 아이템 버튼의 아이콘이미지
-	ImageIcon foodIcon = new ImageIcon("./resourceFolder/image/store/foodButton.png");
-	ImageIcon gunIcon = new ImageIcon("./resourceFolder/image/store/gunButton.png");
-	ImageIcon netIcon = new ImageIcon("./resourceFolder/image/store/netButton.png");
-	ImageIcon trapIcon = new ImageIcon("./resourceFolder/image/store/trapButton.png");
-
-	// 동물 버튼의 아이콘이미지
-	ImageIcon deerIcon = new ImageIcon("./resourceFolder/image/store/deerButton.png");
-	ImageIcon lionIcon = new ImageIcon("./resourceFolder/image/store/lionButton.png");
-	ImageIcon rabbitIcon = new ImageIcon("./resourceFolder/image/store/rabbitButton.png");
-	ImageIcon tigerIcon = new ImageIcon("./resourceFolder/image/store/tigerButton.png");
+	private Ost ost = new Ost();
+	private int index;
+	private int numAni;
 
 	// 아이템 버튼을 만드는 생성자
 	public StoreButton(Item item) {
@@ -45,28 +37,32 @@ public class StoreButton extends JButton implements ActionListener {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				store.checkMoney(hunter, item);
-				setMoneyLabel();
-				//확인창도 뜨게 가능할것같음!
-				//이 아이템을 구매하시겠습니까? 예/아니오 아이템 가격,개수,구매후 잔액나타내기
+				//setMoneyLabel();
+				// 확인창도 뜨게 가능할것같음!
+				// 이 아이템을 구매하시겠습니까? 예/아니오 아이템 가격,개수,구매후 잔액나타내기
 			}
 		});
 	}
 
 	// 애니멀 버튼을 만드는 생성자
-	public StoreButton(Animal ani) {
-		this.ani = ani;
+	public StoreButton(String name, int index) {
+		this.name = name;
+		this.index = index;
+		// numAni = hunter.getNumAni()[index];
 		this.setSize(120, 120);
 		this.setBorderPainted(false);
 		this.setContentAreaFilled(false);
 		this.setFocusPainted(false);
-		this.checkAnimal(); 
+		this.checkAnimal();
 		this.addActionListener(this);
 		this.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				hunter.sellAni(ani);
+				ani = hunter.sellAni(name);
 				setMoneyLabel();
-				//확인창 이 동물을 파시겠습니까? 예/아니오 동물 마리수, 동물판매 후 잔액 나타내기
+				// 확인창 이 동물을 파시겠습니까? 예/아니오 동물 마리수, 동물판매 후 잔액 나타내기
+				ani.getPrice();
+				int money = hunter.getMoney() - ani.getPrice();
 				checkAnimal();
 			}
 		});
@@ -84,16 +80,17 @@ public class StoreButton extends JButton implements ActionListener {
 	public void setMoneyLabel() {
 		moneyLabel.setText("" + hunter.getMoney());
 	}
-	
+
 	// 리스트에 동물이 없으면 버튼 못누르도록 바꿔주는 메소드
 	public void checkAnimal() {
-		if (this.ani == null) { 
+		if (this.numAni == 0) {
 			this.setEnabled(false);
-		}
+		} else
+			this.setEnabled(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// 클릭 효과음
+		ost.playmusic("./resourceFolder/sound/clickbgm.wav");
 	}
 }
--- src/view/StoreView.java
+++ src/view/StoreView.java
@@ -1,19 +1,30 @@
 package view;
 
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import model.active.Hunter;
+import model.item.Feed;
+import model.item.Gun;
+import model.item.Net;
+import model.item.Trap;
 
 public class StoreView extends JPanel {
 
 	private Hunter h;
-	private Image background = new ImageIcon("./resourceFolder/image/store/store.jpg").getImage();
+	private Image background = new ImageIcon("./resourceFolder/image/store/store.png").getImage();
 	private ViewController F;
 	private StoreButton sb;
+	private Ost ost = new Ost();
 
 	private StoreButton deerB;
 	private StoreButton lionB;
@@ -28,43 +39,45 @@ public class StoreView extends JPanel {
 	// 사냥꾼이 소유한 돈을 나타내는 레이블
 	private JLabel moneyLabel;
 
+	private JButton exit;
+
 	public StoreView(ViewController f) {
 		this.F = f;
 		this.setSize(1280, 720);
 		this.setLayout(null);
 		this.setVisible(true);
-		//동물 버튼들 생성하기
-		deerB = new StoreButton(h.getPrison().get(0));
-		lionB = new StoreButton(h.getPrison().get(1));
-		rabbitB = new StoreButton(h.getPrison().get(2));
-		tigerB = new StoreButton(h.getPrison().get(3));
+		// 동물 버튼들 생성하기
+		deerB = new StoreButton("deer", 0);
+		lionB = new StoreButton("lion", 1);
+		rabbitB = new StoreButton("rabbit", 2);
+		tigerB = new StoreButton("tiger", 3);
 		// -> 감옥에 어떤식으로 동물을 저장하는지 몰라서 일단 이런식으로 넣어놨어요 수정해야할부분일것같음!
 
-		//아이템 버튼들 생성하기
-		foodB = new StoreButton(h.getItems()[3]);
-		gunB = new StoreButton(h.getItems()[2]);
-		netB = new StoreButton(h.getItems()[1]);
-		trapB = new StoreButton(h.getItems()[0]);
+		// 아이템 버튼들 생성하기
+		foodB = new StoreButton(Feed.getIstance());
+		gunB = new StoreButton(Gun.getInstance());
+		netB = new StoreButton(Net.getInstance());
+		trapB = new StoreButton(Trap.getInstance());
 
 		// 버튼 아이콘 이미지 세팅
-		deerB.setIcon(sb.deerIcon);
-		lionB.setIcon(sb.lionIcon);
-		rabbitB.setIcon(sb.rabbitIcon);
-		tigerB.setIcon(sb.tigerIcon);
-		foodB.setIcon(sb.foodIcon);
-		gunB.setIcon(sb.gunIcon);
-		netB.setIcon(sb.netIcon);
-		trapB.setIcon(sb.trapIcon);
+		deerB.setIcon(new ImageIcon("./resourceFolder/image/store/deerButton.png"));
+		lionB.setIcon(new ImageIcon("./resourceFolder/image/store/lionButton.png"));
+		rabbitB.setIcon(new ImageIcon("./resourceFolder/image/store/rabbitButton.png"));
+		tigerB.setIcon(new ImageIcon("./resourceFolder/image/store/tigerButton.png"));
+		foodB.setIcon(new ImageIcon("./resourceFolder/image/store/foodButton.png"));
+		gunB.setIcon(new ImageIcon("./resourceFolder/image/store/gunButton.png"));
+		netB.setIcon(new ImageIcon("./resourceFolder/image/store/netButton.png"));
+		trapB.setIcon(new ImageIcon("./resourceFolder/image/store/trapButton.png"));
 
 		// 버튼 아이콘 위치 지정
-		deerB.setLocation(284, 429);
-		lionB.setLocation(416, 429);
-		rabbitB.setLocation(548, 429);
-		tigerB.setLocation(680, 429);
-		foodB.setLocation(284, 558);
-		gunB.setLocation(416, 558);
-		netB.setLocation(548, 558);
-		trapB.setLocation(680, 558);
+		deerB.setLocation(224, 340);
+		lionB.setLocation(356, 340);
+		rabbitB.setLocation(488, 340);
+		tigerB.setLocation(620, 340);
+		foodB.setLocation(224, 468);
+		gunB.setLocation(356, 468);
+		netB.setLocation(488, 468);
+		trapB.setLocation(620, 468);
 
 		// 패널에 버튼 추가
 		this.add(deerB);
@@ -77,10 +90,28 @@ public class StoreView extends JPanel {
 		this.add(trapB);
 
 		// 돈 레이블 패널에 세팅하기
-		moneyLabel = sb.getMoneyLabel();
-		moneyLabel.setLocation(301, 700);
-		this.add(moneyLabel);
+		// moneyLabel = sb.getMoneyLabel();
+		// moneyLabel.setLocation(301, 700);
+		// this.add(moneyLabel);
 
+		exit = new JButton("나가기");
+		exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				System.out.println("exit click!");
+				ost.playmusic("./resourceFolder/sound/clickbgm.wav");
+			}
+		});
+		exit.setBounds(1200, 700, 100, 20);
+		this.add(exit);
+
+	}
+
+	public void paintComponent(Graphics g) {
+		// Display image at at full size
+		g.drawImage(background, 0, 0, null);
+		setOpaque(false);
+		super.paintComponent(g);
 	}
 
 }
