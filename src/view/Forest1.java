4333b4aefeec14beb9638f88153c1c2020f9d6d8
kyungjin99 <kyungjin.kim.cs@gmail.com>
2019-06-19 00:23:23 +0900

    commit (draft)

--- src/view/Forest1.java
+++ src/view/Forest1.java
@@ -1,79 +1,208 @@
 package view;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.util.ArrayList;
-
-import model.active.Hunter;
-import model.item.*;
-
+import java.util.Timer;
+import java.util.TimerTask;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class Forest1 extends JPanel implements KeyListener, Runnable {
-	
+import model.map.Forest;
+import model.item.*;
+import model.active.*;
+
+public class Forest1 extends JPanel implements KeyListener, Runnable, ActionListener {
 	private ViewController F;
-	private JButton hb; // HunterButton 생성
-	//키보드 입력 처리를 위한 변수들
-	private boolean KeyUp = false; 
+	private Forest fModel;
+	private Image background = new ImageIcon("./resourceFolder/image/forest1_background.png").getImage();
+	
+	// 키보드 입력 처리를 위한 변수들
+	private boolean KeyUp = false;
 	private boolean KeyDown = false;
 	private boolean KeyLeft = false;
 	private boolean KeyRight = false;
+
 	// 타이머 관련 필드
 	private JLabel timerLabel;
-//	Timer timer; // 타이머 스레드 클래스 
-	Thread timerTh; // 실제 타이머 스레드	
-	Thread th; // 움직임 스레드
+	Thread timerTh; 	
+//	Timer m_timer;
+//	TimerTask m_task;
+	
+	Thread th; // 사냥꾼 움직임 스레드
+	
+	// 사냥 스레드
+	CrashTh cTh; // hunter
+	Thread crashTh;
+	CrashTh cTh2; // trap
+	Thread crashTh2;
+	CrashTh cTh3; // hunterdog
+	Thread crashTh3; 
+	
+	private JButton hb; // 사냥꾼
+	private ArrayList<AnimalButton> animals; // 동물 리스트 (각각 4마리씩 생성하기)
+	private ArrayList<Animal> prey; // forest model에서 받아온 동물 리스트
+	private JButton mushroom; // 버섯
+	private JButton hd; // 사냥개
+	
+	// 아이템 선택 패널 관련
+	private JPanel itemPanel;
+	private JLabel itemLabel;
+	private JButton trapBtn, netBtn, gunBtn; // 아이템 선택 패널에서 클릭할 버튼들
+	private JButton trap; // 실제 숲에서의 트랩 이미지 (버튼)
+	
+	// hunter의 사냥상태를 알려줄 변수들: 사용여부 저장
+	private boolean isUsingTrap = false;
+	private boolean isUsingNet = false;
+	private boolean isUsingGun = false;
 
-	private ArrayList<AnimalButton> animals; 
-
-	private JButton trap; // 트랩 (임시)
 	
 	// CONSTRUCTOR ///////////////////////////////////////////////////////////////////
 	public Forest1(ViewController f) {
+		
 		this.setLayout(null);
 		this.setSize(1280,720);
 		this.F = f;
 		
-		// Setting HunterButton
-		hb = new HunterButton(Hunter.getInstance());
-		hb.setSize(180,280);
-		hb.setLocation(100,100);
-		hb.addKeyListener(this); // 키보드 이벤트 실행
-		this.add(hb);
-		
-		// 버튼 움직임 스레드
-		th = new Thread(this);  // 스레드 생성
-	    th.start();  // 스레드 실행
-			
 		// 타이머 스레드 관련 초기화
 		timerLabel = new JLabel();
 		timerLabel.setForeground(Color.RED);
 		timerLabel.setFont(new Font("Comic Sans MS", Font.BOLD | Font.BOLD, 30));
 		timerLabel.setBounds(10, 10, 80, 40);
 		this.add(timerLabel);
-//		timer = new Timer(timerLabel);
-//		timerTh = new Thread(timer);
-	//	timerTh.start(); // 스레드 달고 시작
+		timerTh = new Thread() {
+			@Override
+			public void run() {
+				int n = 30;
+				while(n >= 0) {
+					try {
+						timerLabel.setText(Integer.toString(n));
+						Thread.sleep(1000);
+						n--;
+						if(n < 0) {
+							System.out.println("타이머 스레드 종료");
+							F.getCardLayout().show(F.getContentPane(), "Map");
+							this.interrupt();
+						}
+					} catch(InterruptedException e) {}
+				}
+			}
+		};
+		timerTh.start();
+
+		// 사냥꾼 생성
+		hb = new HunterButton(Hunter.getInstance());
+		hb.setBounds(100, 100, 180, 280);
+		hb.addKeyListener(this); // 키보드 이벤트 실행
+		this.add(hb);
+		th = new Thread(this); // 스레드 생성
+	    th.start();
+
+	    // 동물 생성
+	    animals = new ArrayList<AnimalButton>(); // 동물 버튼 생성용 arraylist (forest1에서만)
+	    prey = new ArrayList<Animal>(); // forest model에서 받아올 동물리스트
+	    fModel = new Forest("Forest1");
+	    fModel.setPrey(prey);
+	    fModel.enter(1);
+	    for(Animal ani: prey) {
+	    	AnimalButton aniB = new AnimalButton((Prey)ani);
+	    	if(ani.getActionInfo().getName().equals("Deer")) {
+	    		ImageIcon deerR= new ImageIcon("./resourceFolder/image/animal/DeerRight.png");
+	    		aniB.setSize(164, 286); // 사슴 이미지 사이즈
+		    	aniB.setIcon(deerR);
+	    	} else {
+		    	ImageIcon rabbitR = new ImageIcon("./resourceFolder/image/animal/RabbitRight.png");
+	    		aniB.setSize(149, 155); // 토끼 이미지 사이즈
+		    	aniB.setIcon(rabbitR);
+	    	}
+	    	
+	    	// 동물 랜덤 배치
+	    	int randX = (int)(Math.random() * 900) + 1;
+	    	int randY = (int)(Math.random() * 350) + 1;
+	    	aniB.setLocation(randX, randY);
+	    	
+	    	// 동물 버튼 움직임 스레드 달기
+	    	MovingTh movingAni = new MovingTh(aniB);
+	    	Thread aniTh = new Thread(movingAni);
+	    	aniTh.start();
+	    	
+	    	animals.add(aniB); 
+	    	this.add(aniB);
+	    }
+
+	    // 버섯 생성
+		mushroom = new AnimalButton(new Mushroom()); 
+		mushroom.setSize(100, 98);
+		mushroom.setLocation(700, 500);
+		mushroom.setIcon(new ImageIcon("./resourceFolder/image/animal/mushroomRight.png"));
+		this.add(mushroom);
+		MovingTh movingMsh = new MovingTh((AnimalButton)mushroom);
+		Thread mshTh = new Thread(movingMsh);
+		mshTh.start();
 		
-		// 아이템 관련 초기화
-//		trap = new ItemButton(new Trap()); // Trap 생성자가 private으로 선언되어있음!
-//		trap.setSize(가로, 세로);
-//		trap.addKeyListener(this); // 키보드 이벤트 실행
-// 		방향키로 이동하다가 ENTER키를 누르는 곳에 setLocation하기
-//		this.add(trap);
-//		this.setVisible(true);
+		// 사냥개 생성
+		hd = new AnimalButton(HunterDog.getInstance());
+		hd.setIcon(new ImageIcon("./resourceFolder/image/animal/HoundRight.png"));
+		hd.setSize(230, 190);
+		hd.setLocation(400, 400);
+		this.add(hd);
+		MovingTh movingHd = new MovingTh((AnimalButton)hd);
+		Thread hdTh = new Thread(movingHd);
+		hdTh.start();
+		
+		// 충돌 스레드 관련 초기화
+		cTh = new CrashTh(hb, animals); // hunter과 동물 충돌
+		crashTh = new Thread(cTh);
+		crashTh.start();
+		cTh2 = new CrashTh(trap, animals); // trap과 동물 충돌
+		crashTh2 = new Thread(cTh2);
+		crashTh2.start();
+		cTh3 = new CrashTh(hd, animals); // hunterdog과 동물 충돌
+		crashTh3 = new Thread(cTh3);
+		crashTh3.start();
+		
+		trap = new JButton(); // 트랩 버튼 생성
+				
+		// 아이템 선택 관련 초기화
+		itemPanel = new JPanel();
+		itemPanel.setBounds(0, 600, 1280, 120);
+		itemPanel.setBackground(Color.YELLOW);
+		this.add(itemPanel);
+		itemLabel = new JLabel();
+		itemLabel.setForeground(Color.BLACK);
+		itemLabel.setFont(new Font("Comic Sans MS", Font.BOLD | Font.BOLD, 20));
+		itemLabel.setBounds(10, 630, 200, 30);
+		itemLabel.setText("Select item!");
+		itemPanel.add(itemLabel);
+		trapBtn = new JButton("Trap"); 
+		trapBtn.setBounds(400, 630, 50, 50);
+		trapBtn.addActionListener(this);
+		itemPanel.add(trapBtn);
+		netBtn = new JButton("Net"); 
+		netBtn.setBounds(600, 630, 50, 50); 
+		netBtn.addActionListener(this);
+		itemPanel.add(netBtn);
+		gunBtn = new JButton("Gun"); 
+		gunBtn.setBounds(800, 630, 50, 50); 
+		gunBtn.addActionListener(this);
+		itemPanel.add(gunBtn);	
+		itemPanel.setVisible(true);
 
 
+		this.setVisible(true);
 	}
 	//////////////////////////////////////////////////////////////////////////////////
 	
 	
-	// 움직임 관련 메소드 //////////////////////////////////////////////////////////////////
-	public void keyTyped(KeyEvent e) {
-		 
-	 }
+	// hb의 움직임 관련 메소드 /////////////////////////////////////////////////////////////
+	@Override public void keyTyped(KeyEvent e) {}
+	@Override
 	public void keyPressed(KeyEvent e) {
 		switch(e.getKeyCode()){
 			case KeyEvent.VK_UP :
@@ -84,16 +213,44 @@ public class Forest1 extends JPanel implements KeyListener, Runnable {
 	    		break;
 	    	case KeyEvent.VK_LEFT :
 	    		KeyLeft = true;
-	    		((HunterButton) hb).PressLeft();
+	    		if(isUsingNet) {
+	    			hb.setSize(270, 270);
+	    			hb.setIcon(new ImageIcon("./resourceFolder/image/hunter/tubeNetLeft.png"));
+	    			System.out.println("net - left"); // test
+	    		}
+	    		else if(isUsingGun){
+	    			hb.setSize(270, 270);
+	    			hb.setIcon(new ImageIcon("./resourceFolder/image/hunter/tubeGunLeft.png"));
+	    			System.out.println("gun - left"); // test
+	    		}
+	    		else { // trap || w/o item
+	    			hb.setSize(180, 280);
+	    			hb.setIcon(new ImageIcon("./resourceFolder/image/hunter/tubeLeft1.png"));
+	    			System.out.println("default - left");
+	    		}
 	    		break;
 	    	case KeyEvent.VK_RIGHT :
 	    		KeyRight = true;
-	    		((HunterButton) hb).PressRight();
+	    		if(isUsingNet) {
+	    			hb.setSize(270, 270);
+	    			hb.setIcon(new ImageIcon("./resourceFolder/image/hunter/tubeNetRight.png"));
+	    			System.out.println("net - right"); // test
+	    		}
+	    		else if(isUsingGun){
+	    			hb.setSize(270, 270);
+	    			hb.setIcon(new ImageIcon("./resourceFolder/image/hunter/tubeGunRight.png"));
+	    			System.out.println("gun - right"); // test
+	    		}
+	    		else { // trap || w/o item
+	    			hb.setSize(180, 280);
+	    			hb.setIcon(new ImageIcon("./resourceFolder/image/hunter/tubeRight1.png"));
+	    			System.out.println("default - right");
+	    		}
 	    		break;
-	    	default:
-	    		break;
+	    	default: break;
 	    }
 	}
+	@Override
 	public void keyReleased(KeyEvent e){
 		switch(e.getKeyCode()){
 	    	case KeyEvent.VK_UP :
@@ -117,42 +274,150 @@ public class Forest1 extends JPanel implements KeyListener, Runnable {
 		 if(KeyLeft == true) hb.setBounds(hb.getX()-5,hb.getY(), hb.getWidth(),hb.getHeight());
 		 if(KeyRight == true) hb.setBounds(hb.getX()+5,hb.getY(), hb.getWidth(),hb.getHeight());
 	 }
-	// AnimalButton들을 랜덤으로 움직이게 하는 메소드
-	public void SetAniMvt(JButton p) { 
-		int rand = (int) (Math.random() * 100 + 1); // 0.25의 확률로 상하좌우로 움직이도록 설정
-		if(rand <= 25) p.setLocation(p.getX(), p.getY() - 5); 						// UP
-		else if(rand > 25 && rand <= 50) p.setLocation(p.getX(), p.getY() + 5); 	// DOWN
-		else if(rand > 50 && rand <=75) p.setLocation(p.getX() - 5, p.getY());		// LEFT
-		else p.setLocation(p.getX() + 5, p.getY()); 								// RIGHT
-	}  
 	// HunterButton 이동 스레드
 	public void run() {
 		try {
-			while(true){ 
+			while(true){
 				KeyProcess(); // 키보드 입력처리를 하여 x,y 갱신
-				Thread.sleep(20); // 20 대신 hunter의 speed가 들어감
-				// Thread.sleep(hb.h.getSpeed()) ??
+				Thread.sleep(Hunter.getInstance().getActionInfo().getDelay());
 			}
 		} catch (Exception e){}
-	}
-	// HunterButton을 제외한 나머지 버튼들을 자동으로 이동시키는 스레드
-	public void run(JButton prey) { 
-		try {
-			SetAniMvt(prey);
-			Thread.sleep(100); // 100 대신 prey의 speed가 들어감
-			// Thread.sleep(prey.p.getSpeed()) ??
-		} catch(Exception e) {}
 	}
 	
 	
 	// 사냥하기 ////////////////////////////////////////////////////////////////////////
 	
-	// 0. 두 버튼의 충돌 여부 파악하기
-	// JButton은 화면에 띄우기 전까지 좌표와 크기가 없으므로 각 버튼클래스 내부에서 좌표비교를 통해 충돌을 파악할 수 X
-	// --> 숲에서 충돌을 감지하고 각 버튼에 충돌여부를 넘겨주기
-	// 최종 action은 숲에서 처리
+	// 아이템을 활용해 사냥하는 경우: 아이템버튼 클릭
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Item[] currentItem = Hunter.getInstance().getItems();
+		// 버튼을 누르고 사냥꾼이 해당 아이템을 구매했으면 실행
+		if(e.getSource() == trapBtn && currentItem[0] != null && isUsingTrap == false) { 
+			setTrap();
+		}
+		else if(e.getSource() == netBtn && currentItem[1] != null && isUsingNet == false) {
+			setNet();
+		}
+		else if(e.getSource() == gunBtn &&  currentItem[2] != null && isUsingGun == false) {
+			setGun();
+		}
+	}
+
+	// 트랩 설치하기
+	public void setTrap() {
+		// 트랩 이미지 설정
+		trap.setSize(150, 150);
+		ImageIcon trapB = new ImageIcon("./resourceFolder/image/store/trapButton.png");
+		trap.setIcon(trapB);
+		trap.setBorderPainted(false);
+    	trap.setContentAreaFilled(false);
+    	trap.setFocusPainted(false);
+    	
+    	trap.setVisible(false); // 위치를 선택하기 전까지 안보이도록
+    	this.add(trap);
+    	
+		// 프레임 상에서 원하는 위치에 마우스를 클릭하면 트랩을 설치
+		this.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int x = e.getX();
+				int y = e.getY();
+				trap.setLocation(x, y);
+				trap.setVisible(true);
+				removeMouseListener(this); // 설치한 다음에는 리스너 제거
+				System.out.println("trap is set! - mouse listener is removed");
+			}
+			@Override public void mouseEntered(MouseEvent e) {}
+			@Override public void mouseExited(MouseEvent e) {}
+			@Override public void mousePressed(MouseEvent e) {}
+			@Override public void mouseReleased(MouseEvent e) {}
+		});
+		this.isUsingTrap = true;
+//		this.isUsingNet = false;
+//		this.isUsingGun = false;
+		this.setVisible(true); // 설치한 다음에 보여주기
+
+		// trap에 crash 스레드 달기
+		cTh2 = new CrashTh(trap, animals);
+		crashTh2 = new Thread(cTh2);
+		crashTh2.start();
+		
+		// 트랩 사용 내역을 전달
+		Hunter.getInstance().useItem(Trap.getInstance());
+	}	
+	
+	// 그물과 함께 사냥하기: hunter의 이미지를 바꿔 사냥꾼의 range를 넓혀주기
+	public void setNet() {
+		// hunter 이미지 바꿔주기
+		ImageIcon netR = new ImageIcon("./resourceFolder/image/hunter/tubeNetRight.png");
+		ImageIcon netL = new ImageIcon("./resourceFolder/image/hunter/tubeNetLeft.png");
+		hb.setSize(270, 270);
+		hb.setIcon(netR);
+		this.isUsingNet = true;
+//		this.isUsingTrap = false;
+//		this.isUsingGun = false;
+		
+		// 그물 사용 내역을 전달
+		Hunter.getInstance().useItem(Net.getInstance());
+		System.out.println("hunt with a net"); // test
+	}
+	
+	// 총으로 사냥하기
+	public void setGun() {
+		// hunter의 이미지 바꿔주기
+		ImageIcon gunR = new ImageIcon("./resourceFolder/image/hunter/tubeGunRight.png");
+		ImageIcon gunL = new ImageIcon("./resourceFolder/image/hunter/tubeGunLeft.png");
+		hb.setSize(270, 270);
+		hb.setIcon(gunR);
+		// 모든 동물 버튼에 마우스 리스너 달기
+		for(AnimalButton aniB: animals) { 
+			aniB.addMouseListener(new MouseListener() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					System.out.println("gun - hunt success!");
+					HuntSuccess(aniB);
+					removeMouseListener(this); // 사냥된 다음에는 리스너 제거
+				}
+				@Override public void mouseEntered(MouseEvent arg0) {}
+				@Override public void mouseExited(MouseEvent arg0) {}
+				@Override public void mousePressed(MouseEvent arg0) {}
+				@Override public void mouseReleased(MouseEvent arg0) {}				
+			});
+		}
+		this.isUsingGun = true;
+//		this.isUsingTrap = false;
+//		this.isUsingNet = false;
+		// 총 사용 내역을 전달해주기
+		Hunter.getInstance().useItem(Gun.getInstance());
+		System.out.println("hunt with a gun"); // test
+	}
+	
+
+	// 사냥 성공 후 동물 처리 ////////////////////////////////////////////////////////////////////////
+	public void HuntSuccess(AnimalButton aniB) {
+		aniB.setCaptured(true); // animal의 포획상태를 저장
+		Hunter.getInstance().getPrison().add(aniB.getAni()); // hunter의 prison에 동물 넣기
+		System.out.println("prison에 저장!");
+		for(int i = 0; i < Hunter.getInstance().getPrison().size(); i++) { // prison에 잘 들어갔는지 test
+			System.out.print(i + ": " + Hunter.getInstance().getPrison().get(i).getActionInfo().getName() + " ");
+		}
+		System.out.println();
+		aniB.setVisible(false);
+		aniB.setEnabled(false); // 잡힌 동물의 버튼이 사라지게 하기
+		this.remove(aniB);
+	}
+	
+	
+	// 바탕화면 구성 //////////////////////////////////////////////////////////////////////////////
+	public void paintComponent(Graphics g) {
+		g.drawImage(background, 0, 0, null);
+		setOpaque(false);
+		super.paintComponent(g);
+	}
+
+
 	public boolean isCrash(JButton b1, AnimalButton b2) {
-		// b1에는 사냥하는 주체(사냥꾼|사냥개|trap)가, b2에는 사냥감이 들어감
+		// b1에는 사냥하는 주체(사냥꾼|사냥개|trap)가, b2에는 동물(사냥감|버섯)이 들어감
 		int b1LowerLeftX = b1.getLocation().x;
 		int b1LowerLeftY = b1.getLocation().y - b1.getHeight();
 		int b2LowerLeftX = b2.getLocation().x;
@@ -167,35 +432,23 @@ public class Forest1 extends JPanel implements KeyListener, Runnable {
 		}
 		else return false;
 	}
-	
-	// Case 1. 사냥꾼이 동물을 직접 사냥: HunterButton과 동물버튼의 crash가 일어날 때
-	public void hunt() { 
+	public void checkCrash() {
 		for(AnimalButton aniB: animals) {
 			if(isCrash(hb, aniB)) {
-				if(aniB.getTag().equals("mushroom")) { // 사냥꾼이 버섯이랑 만날경우
-					// 사냥꾼의 speed 증가
+				if(aniB.getAni().getActionInfo().getName().equals("머시룸")) {
+					aniB.getMush().heal(Hunter.getInstance()); // 사냥꾼의 speed 증가
+					System.out.println("버섯과 crash 발생"); // test
 				}
-				else if(aniB.getTag().equals("prey")) { // 사냥꾼이 사냥감과 만날경우
-					// hb.h.catchAni(aniB.ani); 해당 AnimalButton이 가지고 있는 animal을 잡기
+				else if((aniB.getAni().getActionInfo().getName().equals("Deer") || 
+						aniB.getAni().getActionInfo().getName().equals("Rabbit") || 
+						aniB.getAni().getActionInfo().getName().equals("Tiger") || 
+						aniB.getAni().getActionInfo().getName().equals("Lion")) 
+						&& !aniB.isCaptured()) { // 이미 잡은 동물 계속 crash 탐지하는 것 방지
+					Hunter.getInstance().catchAni(aniB.getAni()); // 해당 AnimalButton이 가지고 있는 animal을 잡기
+					System.out.println("crash with animal"); // test
 				}
-			}			
-			// 2-1. 트랩을 사용해 사냥
-			if(isCrash(trap, aniB)) {
-				// 
 			}
 		}
 	}
-	
-//	Case 2. 사냥꾼이 아이템을 이용해 동물을 직접 사냥
-	// hb.h.useItem(trap)을 선택하면
-//	2-2. 그물을 사용해 사냥
-//			사냥꾼의 range를 넓혀줌
-	
-//	2-3. 총을 사용해 사냥
-//				
-
-	
-//	사냥 성공 시 잡힌 prey의 버튼이 사라지게 하기
-	
 
 }
--- src/view/HunterButton.java
+++ src/view/HunterButton.java
@@ -6,28 +6,38 @@ import model.active.Hunter;
 
 public class HunterButton extends JButton {
     private Hunter h;
-    private ImageIcon tubeR = new ImageIcon("./resourceFolder/image/tubeRight1.png");
-    private ImageIcon tubeL = new ImageIcon("./resourceFolder/image/tubeLeft1.png");
+    private ImageIcon tubeR = new ImageIcon("./resourceFolder/image/hunter/tubeRight1.png");
+    private ImageIcon tubeL = new ImageIcon("./resourceFolder/image/hunter/tubeLeft1.png");
 
     //HunterButton(Hunter h)로 바꾸기, this.h=h; 주석해제 처리하기
     public HunterButton(Hunter h) {
         this.h = h;
         //this.setText(h.getName());
-
+        
         this.setIcon(tubeR);
         this.setBorderPainted(false);
         this.setContentAreaFilled(false);
         this.setFocusPainted(false);
         this.setVisible(true);
     }
-
-   
-
+    
     public void PressRight() {
         this.setIcon(tubeR);
     }
-
     public void PressLeft() {
         this.setIcon(tubeL);
     }
+    public void PressRight(ImageIcon img) {
+    	this.setIcon(img);
+    }
+    public void PressLeft(ImageIcon img) {
+    	this.setIcon(img);
+    }
+    public void PressRight(int index) {
+    	this.setIcon(new ImageIcon(h.getBtmImg(index)));
+    }
+    public void PressLeft(int index) {
+    	this.setIcon(new ImageIcon(h.getBtmImg(index)));
+    }
+    
 }
